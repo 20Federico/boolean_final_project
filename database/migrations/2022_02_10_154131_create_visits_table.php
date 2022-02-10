8d40dc69eb152c54e22356ebc27a3836b56d77e4
20Federico <fefe.molino@gmail.com>
2022-02-10 16:51:31 +0100

    created visits table + model + relation with apartment

---
+++ database/migrations/2022_02_10_154131_create_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('n_visits')->default(0);
+            $table->string('ip_address');
+            $table->foreignId('apartment_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
