699a2a670720c9527210b4dde998eaa9840e7626
Mega-117 <enricosaian@hotmail.it>
2022-02-11 15:08:42 +0100

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/create', 'HomeController@create')->name("create");
 
 Auth::routes();
 
