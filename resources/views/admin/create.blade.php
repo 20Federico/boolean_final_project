4d448a0d165b89d001c428a784baadf25a44c364
Mega-117 <enricosaian@hotmail.it>
2022-02-11 13:37:07 +0100

    impostato form

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -8,14 +8,38 @@
         
         <div class="mb-3">
             <label for="title" class="form-label" >Titolo Appartamento</label>
-            <input type="text" class="form-control" name="title" id="field_title">
-            <br>
+            <input type="text" class="form-control" name="title" id="title">
         </div>
         <div class="mb-3">
-            <label for="series" class="form-label">serie</label>
-            <input type="texarea" class="form-control" name="series" id="field_series">
-            <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
-            
+            <label for="description" class="form-label">Descrizione</label>
+            <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="cover_img" class="form-label">Immagine</label>
+            <input type="file" name="cover_img" id="cover_img" class="form-control" value="">
+        </div>
+        <div class="mb-3">
+            <label for="price_day" class="form-label" >Prezzo a notte</label>
+            <input type="text" class="form-control" name="price_day" id="price_day">
+        </div>
+        <div class="mb-3">
+            <label for="n_rooms" class="form-label" >Numero stanze</label>
+            <input type="text" class="form-control" name="n_rooms" id="n_rooms">
+        </div>
+        <div class="mb-3">
+            <label for="n_baths" class="form-label" >Numero Bagni</label>
+            <input type="text" class="form-control" name="n_baths" id="n_baths">
+        </div>
+        <div class="mb-3">
+            <label for="n_baths" class="form-label" >Dimensioni (Metri quadri)</label>
+            <input type="text" class="form-control" name="n_baths" id="n_baths">
+        </div>
+        <div class="mb-3">
+            <label for="n_baths" class="form-label" >Casa condivisa</label>
+            <select name="cars" id="cars">
+                <option value="true">Si</option>
+                <option value="false">No</option>
+              </select>
         </div>
     
     <button type="reset" class="btn btn-primary">Reset</button>
