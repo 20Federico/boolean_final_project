f26b1dbb56a763ac3fe0e3a6187250f904fa8ef6
Riccardo-Grasso <riccardo.grasso@outlook.it>
2022-02-18 12:12:38 +0100

    favicon + home responsive

--- resources/views/layouts/guests.blade.php
+++ resources/views/layouts/guests.blade.php
@@ -12,6 +12,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         <title>@yield('title')</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}">
 
         <!-- Styles -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -75,13 +76,13 @@
             @yield('content')
         </main>
 
-        <footer class="py-4 bg-light mt-auto">
+        <footer class="py-4 bg-light mt-auto border-top">
             <div class="container">
                 <div>
-                    <div class="row row-cols-4">
-                        <div class="col"><strong>Assistenza</strong>
-                            <div class="mt-3">
-                                <ul class="assistenza">
+                    <div class="row row-cols-1 row-cols-sm-4 d-flex flex-column flex-sm-row">
+                        <div class="col ps-4"><strong>Assistenza</strong>
+                            <div class="mt-3 border-bottom w-100">
+                                <ul class="assistenza w-100">
                                     <li><a href="#">Centro Assistenza</a></li>
                                     <li><a href="#">Informazioni di sicurezza</a></li>
                                     <li><a href="#">Opzioni di cancellazione</a></li>
@@ -89,29 +90,29 @@
                                 </ul>
                             </div>
                         </div>
-                        <div class="col"><strong>Community</strong>
-                            <div class="mt-3">
-                                <ul class="community">
+                        <div class="col ps-4 mt-3 mt-sm-0"><strong>Community</strong>
+                            <div class="mt-3 border-bottom border-bottom w-100">
+                                <ul class="community w-100">
                                     <li><a href="#">Airbnb.org: un rifugio sicuro</a></li>
                                     <li><a href="#">Tutte le info sulla nostra community</a></li>
                                     <li><a href="#">Entra nella community</a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div class="col"><strong>Ospitare</strong>
-                            <div class="mt-3">
-                                <ul class="ospitare">
+                        <div class="col ps-4 mt-3 mt-sm-0"><strong>Ospitare</strong>
+                            <div class="mt-3 border-bottom w-100">
+                                <ul class="ospitare w-100">
                                     <li><a href="#">Prova a ospitare</a></li>
                                     <li><a href="#">AirCover: host protetti</a></li>
                                     <li><a href="#">Esplora le risorse per host</a></li>
                                     <li><a href="#">Vai al nostro forum</a></li>
-                                    <li><a href="#">Come ospitare responsabilmente</a></li>
+                                    <li><a href="#">Come ospitare</a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div class="col"><strong>Informazioni</strong>
-                            <div class="mt-3">
-                                <ul class="informazioni">
+                        <div class="col ps-4 mt-3 mt-sm-0"><strong>Informazioni</strong>
+                            <div class="mt-3 w-100">
+                                <ul class="informazioni w-100">
                                     <li><a href="#">Newsroom</a></li>
                                     <li><a href="#">Scopri le nuove funzionalità</a></li>
                                     <li><a href="#">Lettera dai nostri fondatori</a></li>
@@ -125,20 +126,19 @@
                     </div>
                     <hr>
                 </div>
-                <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright &copy; Your BoolBnB 2022</div>
-
-                    <div class="social">
-                        <a href="https://it-it.facebook.com/"><i class="fab fa-facebook fa-lg me-2"></i></a>
-                        <a href="https://www.instagram.com/boolbnb/"><i class="fab fa-instagram fa-lg me-2"></i></a>
-                        <a href="https://twitter.com/bn_bool"><i class="fab fa-twitter fa-lg"></i></a>
-                    </div>
-
+                <div class="d-flex flex-column flex-md-row align-items-center justify-content-between small">
                     <div class="privacy">
                         <a href="#">Privacy Policy</a>
                         &middot;
                         <a href="#">Terms &amp; Conditions</a>
                     </div>
+
+                    <div class="social my-4">
+                        <a href="https://it-it.facebook.com/"><i class="fab fa-facebook fa-lg me-2"></i></a>
+                        <a href="https://www.instagram.com/boolbnb/"><i class="fab fa-instagram fa-lg me-2"></i></a>
+                        <a href="https://twitter.com/bn_bool"><i class="fab fa-twitter fa-lg"></i></a>
+                    </div>
+                    <div class="text-muted">Copyright &copy; Your BoolBnB 2022</div>
                 </div>
             </div>
         </footer>
