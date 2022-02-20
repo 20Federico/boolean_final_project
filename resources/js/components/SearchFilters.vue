133400f519aec7f7e96b17d3af2b6754c099e1c3
20Federico <fefe.molino@gmail.com>
2022-02-20 16:01:51 +0100

    crated filters component

---
+++ resources/js/components/SearchFilters.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mb-3">
+    <form action="#" class="py-4">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-8">
+          <label class="fs-4" for="distance">20km - 100km</label>
+          <input id="distance" class="form-range" type="range" min="10" max="100" step="5" value="20">
+        </div>
+      </div>
+      <div class="row justify-content-center align-items-center mb-3">
+        <div class="col-2">
+          <label class="fs-4" for="rooms">min. rooms</label>
+          <input id="rooms" class="form-control" type="number" min="1" max="20" value="3">
+        </div>
+        <div class="col-2">
+          <label class="fs-4" for="beds">min. beds</label>
+          <input id="beds" class="form-control" type="number" min="1" max="20" value="1">
+        </div>
+        <div class="col-2">
+          <label class="fs-4" for="services">Services</label>
+          <select id="services" class="form-select" multiple>
+            <option value="1">Wi-fi</option>
+            <option value="2">Posto Macchina</option>
+            <option value="3">Piscina</option>
+            <option value="4">Portineria</option>
+            <option value="5">Sauna</option>
+            <option value="6">Colazione inclusa</option>
+            <option value="7">Vista mare</option>
+          </select>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary">Applica Filtri</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchFilters'
+}
+</script>
+
+<style lang="scss" scoped>
+
+  form {
+    color: #373a36;
+    background-color: #d48166bd;
+    border-radius: 10px;
+  }
+
+</style>
