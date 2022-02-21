276d73aff27123cca03c424c6d0080e0dc4e2dac
20Federico <fefe.molino@gmail.com>
2022-02-21 01:29:33 +0100

    update

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -3,13 +3,11 @@
 @section('title', 'BoolBnB - Affittare casa non è mai stato così facile!')
 
 @section('content')
-  <hero-section @search="searching = true" :searching='searching'></hero-section>
+  <hero-section @query="getQuery" @search="searching = true" :searching='searching'></hero-section>
 
   <home-page v-if="!searching"></home-page>
 
-  <searched-view v-if="searching" @back="searching = false" :apartments="{{json_encode($apartments)}}" ></searched-view>
-
-  {{-- <div style="height: 500px" class="mymap" id="mymap"></div> --}}
+  <searched-view v-if="searching" @back="searching = false" :apartments="{{json_encode($apartments)}}" :search='search' ></searched-view>
 
 
 @endsection
