dadd97e7eff27a606e139e279be53205a8948bd1
Mega-117 <enricosaian@hotmail.it>
2022-02-21 14:34:40 +0100

    inserito card appartamenti in evidenzza

--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -1,20 +1,101 @@
 <template>
   <div class="container py-5">
-    <h1>
-      HomePage con appartamenti in evidenza e molto altro...
-    </h1>
+    <div class="row justify-content-center gap-3 text-center">
+      <h2>Appartamenti in primo piano</h2>
+      <div class="card px-0" style="width: 18rem">
+        <img
+          src="https://e8rbh6por3n.exactdn.com/sites/uploads/2020/05/villa-la-gi-thumbnail.jpg?strip=all&lossy=1&ssl=1"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body text-center">
+          <h5 style="color: #d48166">Nome Appartamento</h5>
+
+          <hr class="mt-3 mb-3" style="width: 25%; margin: auto" />
+
+          <p class="card-text text-center">
+            <i class="fas fa-map-marker-alt fs-5" style="color: #d48166"></i>
+
+            Via roma 83
+          </p>
+        </div>
+      </div>
+
+      <div class="card px-0" style="width: 18rem">
+        <img
+          src="https://cdn.villa-bali.com/cache/fullSize/villas/villa-umara/villa-umara-57-umara-pool-daylight-a2-5d64dfce678ae.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body text-center">
+          <h5 style="color: #d48166">Nome Appartamento</h5>
+
+          <hr class="mt-3 mb-3" style="width: 25%; margin: auto" />
+
+          <p class="card-text text-center">
+            <i class="fas fa-map-marker-alt fs-5" style="color: #d48166"></i>
+
+            Via roma 83
+          </p>
+        </div>
+        <dir>
+          <!-- scopri Boolbnb -->
+        </dir>
+        <div>
+          <!-- card opzioni -->
+        </div>
+      </div>
+
+      <div class="card px-0" style="width: 18rem">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCqp4kOV4rN0HAmR4kfa12rpdILdl3qmAVRA&usqp=CAU"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body text-center">
+          <h5 style="color: #d48166">Nome Appartamento</h5>
+
+          <hr class="mt-3 mb-3" style="width: 25%; margin: auto" />
+
+          <p class="card-text text-center">
+            <i class="fas fa-map-marker-alt fs-5" style="color: #d48166"></i>
+
+            Via roma 83
+          </p>
+        </div>
+      </div>
+
+      <div class="card px-0" style="width: 18rem">
+        <img
+          src="https://www.affittovacanze.com/images/photos/houses/0464-XL.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body text-center">
+          <h5 style="color: #d48166">Nome Appartamento</h5>
+
+          <hr class="mt-3 mb-3" style="width: 25%; margin: auto" />
+
+          <p class="card-text text-center">
+            <i class="fas fa-map-marker-alt fs-5" style="color: #d48166"></i>
+
+            Via roma 83
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage'
-}
+  name: "HomePage",
+};
 </script>
 
 <style lang="scss" scoped>
-  h1 { 
-    margin-bottom: 500px;
-    margin-top: 100px;
-  }
+h1 {
+  margin-bottom: 500px;
+  margin-top: 100px;
+}
 </style>
