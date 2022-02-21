4ae1ec670d43692099fecb2e4f72781d3d21836d
20Federico <fefe.molino@gmail.com>
2022-02-21 10:23:56 +0100

    added tomtom

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,9 +7,11 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        
+        {{-- tomtom --}}
+        <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.18.0/maps/maps.css' />
+        <script src='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.18.0/maps/maps-web.min.js'></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
         <title>@yield('title')</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}">
@@ -201,5 +203,8 @@
             </div>
         </div>
       </div>
+      <script src="{{ asset('js/app.js') }}"></script>
+      <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/services/services-web.min.js"></script>
+      @yield('extra_scripts')
     </body>
 </html>
