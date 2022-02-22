b51a91213146d1e1985f09768cfb8ad03f1f1d33
20Federico <fefe.molino@gmail.com>
2022-02-22 16:24:15 +0100

    managed payment

--- app/Http/Controllers/Admin/SponsorController.php
+++ app/Http/Controllers/Admin/SponsorController.php
@@ -6,6 +6,11 @@ use App\Apartment;
 use App\Http\Controllers\Controller;
 use App\Sponsor;
 use Illuminate\Http\Request;
+use Braintree;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class SponsorController extends Controller
 {
@@ -16,21 +21,21 @@ class SponsorController extends Controller
      */
     public function index(Apartment $apartment)
     {
-        $sponsorList = Sponsor::all();
-        return view('admin.sponsor.index', [
-            "sponsorList" => $sponsorList,
-            "apartment" => $apartment
-        ]);
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+      if($apartment->user_id != Auth::id()){
+        return abort(401);
+      }
+
+
+        $sponsorList = Sponsor::all();
+        $clientToken = $this->gateway()->clientToken()->generate();
+
+        // return view('admin.sponsor.index', [
+        //     "apartment" => $apartment,
+        //     "sponsorList" => $sponsorList,
+        //     "clientToken" => $clientToken
+        // ]);
+        return view('admin.sponsor.index', compact('apartment', 'sponsorList', 'clientToken' ));
     }
 
     /**
@@ -39,54 +44,73 @@ class SponsorController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Apartment $apartment)
     {
-        //
+      // Get data from payment form
+      $data = $request->all();
+
+      // Error message if no sponsorship has been selected 
+      $sponsorshipError = 'Nessuna sponsorship selezionata.';
+      if(empty($data['amount'])){
+        return redirect()->back()->with('sponsorshipError', $sponsorshipError);
+      }
+
+      // Get apartment info
+      $apartment_id = $apartment->id;
+      
+      // Get Sponsor info
+      $sponsor_id = $data['amount'];
+      $sponsor = Sponsor::where('id', $sponsor_id)->first();
+      $sponsor_duration = $sponsor->duration_hours;
+      $sponsor_price = $sponsor->price_euro;
+
+
+      // Config Transaction
+      $result = $this->gateway()->transaction()->sale([
+        'amount' => $sponsor_price,
+        'paymentMethodNonce' => $data['payment_method_nonce'],
+        'options' => [
+            'submitForSettlement' => true
+        ]
+      ]);
+
+      // Check on Transiction
+      if ($result->success || !is_null($result->transaction)) {
+        $transaction = $result->transaction;
+        
+        // Get DeadLine
+        $deadline = Carbon::now()->addHours($sponsor_duration);
+
+        // Get Transaction id
+        $transId = $transaction->id; 
+        
+        // Popolate Pivot sponsor_apartment Table
+        $newPlace = new Apartment();    
+        $newPlace->sponsor()->attach($apartment_id,
+                                    [
+                                    'expiry'=> $deadline,
+                                    'transaction_id'=> $transId,
+                                    'apartment_id' => $apartment_id,
+                                    'sponsor_id' => $sponsor_id
+                                    ]);
+
+      } else {
+        abort(404);
+      }
+    
+      // return redirect()->route('user.myplace.index')->with('transId',$transId)->with('placeBuy',$place->title);
+      Session::flash('message', 'Transaction Approved');
+      return redirect()->route('admin.apartments.index');
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Sponsor $sponsor)
-    {
-
-        return view('admin.sponsor.show', compact("sponsor"));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    private function gateway(){
+      $gateway = new Braintree\gateway ([
+        'environment' => getenv('BT_ENVIRONMENT'),
+        'merchantId' => getenv('BT_MERCHANT_ID'),
+        'publicKey' => getenv('BT_PUBLIC_KEY'),
+        'privateKey' => getenv('BT_PRIVATE_KEY')
+        ]);
+      return $gateway;
     }
 }
