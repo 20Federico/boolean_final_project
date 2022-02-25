59412814ebbf5a53176c5f2dc520787f516dafce
Mega-117 <enricosaian@hotmail.it>
2022-02-25 12:17:25 +0100

    inserita scadenza nel index apartment

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -26,122 +26,7 @@
         </div>
         @endif
         @foreach ($apartmentsList as $apartment)
-            {{-- <li class="list-group-item {{$apartment->visible == false ? 'list-group-item-secondary' : ''}}">
-              <div class="row row-cols-md-6 row-cols-1 align-items-center">
-                <a href="{{ route('admin.visits.show', $apartment->id) }}">Vedi Statistica</a>
-              
-              <div class="col col-md-4" style="text-transform: capitalize;">
-                {{ $apartment->title }}
-              </div>
-              <div class="col col-md-6">
-                <div class="row row-cols-md-3 row-cols-1">
-                  <div class="col col-md-5">
-                    Aggiunto il: 
-                    @php
-                    echo date_format($apartment->created_at, 'd/m/Y');
-                    @endphp
-                  </div>
-                    
-                
-                  <div class="col col-md-4" >
-                    Stato: 
-                    @if ($apartment->visible)
-                    Pubblicato
-                    @else
-                    Privato
-                    @endif
-                  </div>
-                  <div class="col col-md-3">
-                    Prezzo: {{ $apartment->price_day }} €
-                  </div>
-                </div>
-              </div>
-              <div class="col col-md-2 mt-1 mt-md-0">
-                <div class="row">
-                  <div class="col p-md-0">
-                    <a class="btn btn-primary" href="{{ route('admin.apartments.show', $apartment->id) }}">Dettagli</a>
-                  </div>
-                  <div class="col p-0">
-
-                    <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="post">
-                      @csrf
-                      @method('delete')                      
-                    
-                      <button class="btn btn-outline-danger" type="submit" onclick="return confirm('Are you sure you want to delete this appartment? With this apartment all related messages will be deleted')">Elimina</button>
-                    </form>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </li> --}}
-          
-          <!--
-          <div class="container-fluid apartment-card d-flex flex-wrap mb-3 border border-2">
             
-            <div class="row row-cols-1 row-cols-md-6">
-            
-              <div class="col-lg-1 date-apartment d-flex align-items-center pe-xl-3 me-xl-3 border-end border-2">
-                <p class="fw-lighter fs-6 text-center">23/23/2022 <br>
-                ore <br>
-                15:22:22
-                </p>
-              </div>
-              {{-- colonna thumbnail  --}}
-              <div class="col-lg-5">
-                <div class="img-container position-relative">
-                  @if (substr($apartment->cover_img, 0, 4 ) === 'http')
-                      <img src="{{ url($apartment->cover_img) }}" class="img-fluid" alt="">
-                    @else    
-                      <img src="{{ asset('storage/' . $apartment->cover_img) }}" class="img-fluid" alt="">
-                    @endif
-                </div>
-                {{-- colonna apartment-data --}}
-                <div class="apartment-data d-flex flex-column justify-content-center flex-grow-1 ms-xl-3 ps-xl-3 border-2">
-                  <div class="title pb-xl-2 fw-light fs-5">{{ $apartment->title }}</div>
-                  {{-- <div class="new-messages d-flex align-items-center">
-                    <p class="pe-4"> Hai 2 nuovi messaggi</p>
-                    
-                    <a href="#" class="btn btn-orange ">Leggi</a>
-                  </div>   --}}
-                </div>
-              </div>
-
-              <div class="col-lg-2 apartment-data ps-xl-3 border-start border-2 pe-0">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <p>New messages</p>
-        
-                {{-- estrapolare dati DB --}}
-                <p>3</p>
-              </div>
-        
-        
-              <div class="col-lg-1 apartment-data ps-xl-3 border-start border-end border-2">
-                <i class="fa fa-eye" aria-hidden="true"></i>
-                <p>Views</p>
-                {{-- estrapolare dati DB --}}
-                <p>3</p>
-              </div>
-        
-              <div class="col-lg-2 apartment-data border-end border-2">
-                <i class="fa fa-rocket" aria-hidden="true"></i>
-                {{-- Estrapolare dati DB --}}
-                <p>Advanced Plan</p>
-                <p class="fw-lighter fs-7">ends 23/12/21 ore 15:30</p>
-              </div>
-              <div class="col-lg-1 end-card justify-content-end align-items-center d-flex">
-                <div class="modifier-buttons d-flex flex-column">
-                  {{-- completare con dati DB --}}
-        
-                  <a href="{{ route('admin.apartments.edit', $apartment->id) }}" class="btn btn-orange mb-xl-3">Modifica casa</a>
-                  <a href="{{ route('admin.apartments.destroy', $apartment->id) }}" class="btn theme-btn-white">Elimina casa</a>
-                </div>
-              </div>
-
-          
-            </div>
-          </div>
-          -->
           
           <div class="row row-cols-1 row-cols-md-6 align-items-center text-center apartment-card mb-3 orange-border">
             <div class="d-none d-xl-inline-block col-lg-1">
@@ -208,19 +93,33 @@
               <i class="fa fa-rocket" aria-hidden="true"></i>
                 {{-- Estrapolare dati DB --}}
                 <p class="m-0">
-                  
-                    
-                    
-                    
-                  
-                  
+
                   @if(count($apartment->sponsor) == 0)
                     <span>Base</span>
+                    {{ $apartment->pivot }}
                   @else
                     <span>Premium</span>
                   @endif  
+                  
                 </p>
-                <p class="d-none d-lg-inline-block fw-lighter text-small m-0">ends 23/12/21 ore 15:30</p>
+                <p class="d-none d-lg-inline-block fw-lighter text-small m-0">
+                  @foreach ($apartmentSponsoredList as $item)
+                    @foreach ($item as $value)
+                        
+                        @if ($value->apartment_id == $apartment->id)
+                          @php
+                              $time = strtotime($value->expiry);
+
+                              $dateExpiry = date('Y/m/d',$time);
+                              $hourExpiry = date('H:i:s',$time);
+
+                              echo $dateExpiry . "<br>" . $hourExpiry;
+                          @endphp
+                        @endif
+
+                    @endforeach
+                  @endforeach
+                </p>
             </div>
             {{-- Pulsanti --}}
             <div class="col-lg-2 mt-4 mt-md-0 f-shrink-0 2">
