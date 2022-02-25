6c86a18cba80dd2de4808e855fec11c6f716c2ca
20Federico <fefe.molino@gmail.com>
2022-02-25 20:53:26 +0100

    changet v-if to v-show

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
   <hero-section @search="searching = true" :searching='searching'></hero-section>
-  <home-page v-if="!searching" :pluto="{{json_encode($pluto)}}">
+  <home-page v-show="!searching" :pluto="{{json_encode($pluto)}}">
   </home-page>
-  <searched-view v-if="searching" @back="searching = false" :search='search'></searched-view>
+  <searched-view v-show="searching" @back="searching = false" :search='search'></searched-view>
 
 @endsection
